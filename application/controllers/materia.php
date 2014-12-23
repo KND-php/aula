906d416ad27396b16bebcb2cf0dcfa671a8da450
Diegote58 <diegote58@gmail.com>
2014-12-23 19:01:31 -0200

    Arreglo en materias e instalacion de bootstrap en /estilos

--- application/controllers/materia.php
+++ application/controllers/materia.php
@@ -3,13 +3,41 @@ class Materia extends CI_Controller {
 
    public function __construct() {
     parent::__construct();
+    $this->load->model('materia_model');
+    $this->load->helper('url');
+    //$this->load->database('default');
   }
 
    function index()
    {
-
-      //$this->load->view('mivista');
+      $this->listar();     
       
+
+   }
+
+   public function listar_materias(){
+      $materia = $this->materia_model->listar();
+      
+      foreach($materia as $mat){
+        $data[] = $mat;
+      }
+
+    $result = array();
+    $result['title'] = 'Materias';
+    $result['main_content'] = 'materia/materia';
+    $result['data'] = $data;
+    
+    
+    $this->load->view('main_template',$result);
+   }
+
+   public function alta_materia(){
+
+    $data = array('uid' => $uid,
+                  'finicio' => $finicio,
+                  'fcierre' => $fcierre);
+    $this->materia_model->alta_materia($data);
+
    }
    
 }
