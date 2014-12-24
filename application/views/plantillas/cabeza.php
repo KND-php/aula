8722a2be6df839b60352cf96894c89ae4d238416
Danilo Pérez ortega <danilo_p_o@hotmail.com>
2014-12-24 03:36:20 -0300

    vamos todavia

--- application/views/plantillas/cabeza.php
+++ application/views/plantillas/cabeza.php
@@ -5,8 +5,8 @@
     <ul id="menu">
 		<li class="current_page_item">
 		<a href="<?php echo base_url('index.php'); ?>">Inicio</a></li>
-		<li><a href="<?php echo base_url('index.php/usuario/listar_usuarios'); ?> ">Usuarios</a></li>
-		<li><a href="<?php echo base_url('index.php/Materias'); ?> ">Materias</a></li>
+		<li><a href="<?php echo base_url('usuario/listar_usuarios'); ?> ">Usuarios</a></li>
+		<li><a href="<?php echo base_url('materias'); ?> ">Materias</a></li>
 		<li><a href="<?php echo base_url('estilos/estilo.css'); ?> ">exta 1</a></li>
 		<li><a href="<?php echo base_url('estilos/estilo.css'); ?> ">Otros</a></li>
 	</ul>
--- application/views/plantillas/head.php
+++ application/views/plantillas/head.php
@@ -4,7 +4,8 @@
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 	<title>Aprendendiendo</title>
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />  	
+	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" /> 
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('estilos/estilo.css'); ?> ">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 </head>
   
